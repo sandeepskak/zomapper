6222c51eabcdab82ca7b4d97ee147d89c10b8f5d
sandeep <653sandeep@gmail.com>
2017-01-04 21:24:49 +0530

    Plot current location on map after asking user - use window.navigator

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(this.geolocationSuccess, this.geolocationError);
+  }
+
+  geolocationSuccess(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: latitude, lng: longitude},
+      zoom: 16
+    });
+    var markerPosition = {lat: latitude, lng: longitude};
+    var marker = new window.google.maps.Marker({
+      position: markerPosition,
+      map: map
+    });
+  }
+
+  geolocationError() {
+    console.log('Unable to retrieve location');
+  }
+
   render() {
     return (
       <div className="App">
