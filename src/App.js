497453a86f7a85b408d75933f1c790ff12564df2
sandeep <653sandeep@gmail.com>
2017-01-06 10:47:39 +0530

    Render infoWindows on hovering over restaurants

--- src/App.js
+++ src/App.js
@@ -27,16 +27,18 @@ class App extends Component {
       zoom: 16
     });
     this.map = map;
+    this.markers = [];
   }
 
   renderMarker = (options) => {
     let {map, latitude, longitude, icon} = options;
     let markerPosition = {lat: latitude, lng: longitude};
-    new window.google.maps.Marker({
+    let marker = new window.google.maps.Marker({
       position: markerPosition,
       map: map,
       icon: icon
     });
+    this.markers.push(marker);
   }
 
   fetchNearbyRestaurants = (latitude, longitude) => {
@@ -52,12 +54,15 @@ class App extends Component {
     .then(response => response.json())
     .then(data => {
       this.extractLocations(data.nearby_restaurants);
+      this.extractRestaurantDetails(data.nearby_restaurants);
       this.renderNearbyRestaurants(this.listOfLocations);
+      this.createInfoWindows(this.markers, this.restautantDetails);
     })
   }
 
   extractLocations = (restaurants) => {
     this.listOfLocations = [];
+    this.restautantDetails = [];
     for(let restaurant of restaurants) {
       this.listOfLocations.push(restaurant.restaurant.location);
     }
@@ -91,6 +96,45 @@ class App extends Component {
       this.map.setZoom(16);
     } else {
       document.getElementById('autocomplete').placeholder = 'Enter a city';
+    }
+  }
+
+  createInfoWindow = (marker, popupContent) => {
+    let infoWindow = new window.google.maps.InfoWindow();
+    let map = this.map;
+    window.google.maps.event.addListener(marker, 'mouseover', function () {
+      infoWindow.setContent(popupContent);
+      infoWindow.open(map, marker);
+    });
+    window.google.maps.event.addListener(marker, 'mouseout', function () {
+      infoWindow.close();
+    });
+  }
+
+  createInfoWindows = (markers, restautantDetails) => {
+    markers.shift() //removes first element - the location marker of the user
+    for (let [index, marker] of markers.entries()) {
+      let popupContent = '<div id="restaurantContent"><div>' + restautantDetails[index].name + '</div><div>' + restautantDetails[index].cuisines + '</div></div>';
+      this.createInfoWindow(marker, popupContent);
+    }
+  }
+
+  extractRestaurantDetails = (restaurants) => {
+    for(let restaurant of restaurants) {
+      let {
+        name, cuisines, has_online_delivery,
+        is_delivering_now, has_table_booking,
+        book_url, order_url,
+        price_range, user_rating
+      } = restaurant.restaurant;
+      this.restautantDetails.push({
+        name: name, cuisines: cuisines,
+        has_online_delivery: has_online_delivery,
+        is_delivering_now: is_delivering_now,
+        has_table_booking: has_table_booking,
+        book_url: book_url, order_url: order_url,
+        price_range: price_range, user_rating: user_rating
+      });
     }
   }
 
